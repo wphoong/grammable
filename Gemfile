1063ce901c811ad6b6ce0415b66b0d7e736a0561
Wilson <wphoong@gmail.com>
2017-05-06 21:53:42 +0000

    added new and create method, and tested in rspec

--- Gemfile
+++ Gemfile
@@ -55,3 +55,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'devise'
+
+gem 'simple_form'
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,22 @@
 class GramsController < ApplicationController
   
   def index
+    
   end
-  
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+end
+
+private
+
+def gram_params
+  params.require(:gram).permit(:message)
 end
