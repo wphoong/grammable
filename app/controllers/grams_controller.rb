61f3640da0aaa537b0e43bb98bc2c27d7b017d55
Wilson <wphoong@gmail.com>
2017-05-09 02:12:36 +0000

    added user authentication for edit update and destroy

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
   
 
   def index
@@ -29,11 +29,14 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
       return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
 
@@ -48,6 +51,8 @@ class GramsController < ApplicationController
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
 
+    return render_not_found(:forbidden) if @gram.user != current_user
+
     @gram.destroy
     redirect_to root_path
   end
@@ -60,8 +65,8 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status= :not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
