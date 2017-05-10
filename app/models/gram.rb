f6a4a90119d3ddec76312b8cc94a18c7c740c218
Wilson <wphoong@gmail.com>
2017-05-10 02:21:22 +0000

    created comments#create route and configured in rspec and comments controller

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,9 +2,10 @@ class Gram < ApplicationRecord
 
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
 
   mount_uploader :picture, PictureUploader
-  
+
   belongs_to :user
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :grams
+  has_many :comments
   
 end
